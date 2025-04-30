0671ee17aefe5453a0f821ca68249a5b3d153c38
Oktova Yoga Praditia Samosir <oktovaaaaa@gmail.com>
2025-04-30 21:29:29 +0700

    next note migration

--- database/migrations/2025_03_19_112336_create_pesanans_table.php
+++ database/migrations/2025_03_19_112336_create_pesanans_table.php
@@ -6,26 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('pesanans', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->text('daftar_menu');
-            $table->integer('total_harga'); 
-            $table->string('status'); // Status pesanan (misalnya: 'pending', 'dibayar', 'selesai')
+            $table->integer('total_harga');
+            $table->string('status');
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
         Schema::dropIfExists('pesanans');
