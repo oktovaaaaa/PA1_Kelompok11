bbd570bacf34c21fb740d07faa207ac6b95d20b5
Oktova Yoga Praditia Samosir <oktovaaaaa@gmail.com>
2025-05-01 09:20:06 +0700

    cluear destroy delete for riwayat admin

--- routes/web.php
+++ routes/web.php
@@ -149,7 +149,6 @@ Route::delete('pengumuman/{pengumuman}', [PengumumanController::class, 'destroy'
 
 Route::get('/admin/riwayat-pesanan', [RiwayatadminController::class, 'index'])->name('riwayat.tampilan');
 Route::post('/admin/pesanan/{id}/approve-reject', [RiwayatadminController::class, 'approveRejectPesanan'])->name('admin.approveRejectPesanan');
-Route::delete('Pesanan/{Pesanan}', [UserController::class, 'destroy'])->name('pesanans.destroy');
 
 
 
