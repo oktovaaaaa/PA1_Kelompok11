c68bc7c59654822a10f91870f194335243891502
Oktova Yoga Praditia Samosir <oktovaaaaa@gmail.com>
2025-05-05 18:23:19 +0700

    up model pesanan

--- app/Models/Pesanan.php
+++ app/Models/Pesanan.php
@@ -20,6 +20,6 @@ class Pesanan extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id'); // Tambahkan 'user_id'
+        return $this->belongsTo(User::class, 'user_id'); 
     }
 }
